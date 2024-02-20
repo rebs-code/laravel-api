ac98effff481be9de2745b3a7c25b0ab196c9754
rebs-code <jack.rebussi@gmail.com>
2024-02-20 10:49:10 +0100

    add: seeded data to comments table

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,18 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $new_comment = new Comment();
+
+        //create 50 comments with a loop    
+        for ($i = 0; $i < 50; $i++) {
+
+            $project = Project::inRandomOrder()->first();
+
+            $new_comment = new Comment();
+            $new_comment->project_id = $project->id;
+            $new_comment->author = fake()->name();
+            $new_comment->content = fake()->text();
+            $new_comment->save();
+        }
     }
 }
